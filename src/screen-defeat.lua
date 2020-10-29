0304a550e74f73ead93024e9a368bfeae5f39339
Chris Lane <chris@chris-allen-lane.com>
2020-10-29 19:11:08 -0400

    fix(screens): victory/defeat screen OOB
    Fix a bug that occurred when loading the victory/defeat screens when the
    camera was not on the 0,0 coordinate.

--- src/screen-defeat.lua
+++ src/screen-defeat.lua
@@ -6,4 +6,5 @@ end
 function game.screens.defeat.draw()
     rectfill(0, 0, 127, 127, 12)
     print("game over", 39, 56, 1)
+    camera(0, 0)
 end
--- src/screen-victory.lua
+++ src/screen-victory.lua
@@ -6,4 +6,5 @@ end
 function game.screens.victory.draw()
     rectfill(0, 0, 127, 127, 12)
     print("you win", 39, 56, 1)
+    camera(0, 0)
 end
