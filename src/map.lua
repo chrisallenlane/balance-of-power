47bb6f80533c81f592dd654bdaad24829c273a70
Chris Lane <chris@chris-allen-lane.com>
2020-10-30 19:28:51 -0400

    feat(maps): camera/cursor initializations
    Create a mechanism for spawning the camera/cursor at specified positions
    on map load.

--- src/map.lua
+++ src/map.lua
@@ -1,10 +1,43 @@
 -- specify map coordinates
 game.maps = {
-    {celx = 0, cely = 0, sx = 0, sy = 0, celw = 32, celh = 16},
+    {
+        celx = 0,
+        cely = 0,
+        sx = 0,
+        sy = 0,
+        celw = 32,
+        celh = 16,
+        curx = 1,
+        cury = 1,
+        camx = 0,
+        camy = 0,
+    },
 
-    {celx = 32, cely = 0, sx = 0, sy = 0, celw = 16, celh = 32},
+    {
+        celx = 32,
+        cely = 0,
+        sx = 0,
+        sy = 0,
+        celw = 16,
+        celh = 32,
+        curx = 1,
+        cury = 28,
+        camx = 0,
+        camy = 15,
+    },
 
-    {celx = 48, cely = 0, sx = 0, sy = 0, celw = 32, celh = 32},
+    {
+        celx = 48,
+        cely = 0,
+        sx = 0,
+        sy = 0,
+        celw = 32,
+        celh = 32,
+        curx = 28,
+        cury = 30,
+        camx = 16,
+        camy = 16,
+    },
 }
 
 -- load the specified map
@@ -14,11 +47,11 @@ function game.maps.load(num)
     game.map = game.maps[num]
 
     -- reset the cursor position
-    game.cursor.celx, game.cursor.cely = 0, 0
+    game.cursor.celx, game.cursor.cely = game.map.curx, game.map.cury
 
     -- reset the camera position
-    game.camera.celx, game.camera.cely = 0, 0
-    game.camera.px, game.camera.py = 0, 0
+    game.camera.celx, game.camera.cely = game.map.camx, game.map.camy
+    game.camera.px, game.camera.py = game.map.camx * 8, game.map.camy * 8
 end
 
 -- draw the current map
