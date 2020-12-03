30018c84f960e58153a89faca1b15366e5196a34
Chris Lane <chris@chris-allen-lane.com>
2020-12-02 20:06:39 -0500

    feat(units): animate unit movement
    Implement simple unit movement animations.

--- src/units.lua
+++ src/units.lua
@@ -1,5 +1,15 @@
 game.units = {}
 
+-- update unit coordinates
+function game.units.update()
+    for _, unit in pairs(game.map.units) do
+        if unit.px.x < unit.cell.x * 8 then unit.px.x = unit.px.x + 4 end
+        if unit.px.x > unit.cell.x * 8 then unit.px.x = unit.px.x - 4 end
+        if unit.px.y < unit.cell.y * 8 then unit.px.y = unit.px.y + 4 end
+        if unit.px.y > unit.cell.y * 8 then unit.px.y = unit.px.y - 4 end
+    end
+end
+
 -- draw the units
 function game.units.draw()
     for _, unit in pairs(game.map.units) do
