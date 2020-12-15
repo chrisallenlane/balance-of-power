7e4b4f564120bba412841032a12c2366a4505d42
Chris Lane <chris@chris-allen-lane.com>
2020-12-14 21:13:57 -0500

    refactor(Radius): track cells in Radius area
    - Track which cells are in the radius's area
    
    - Create a `contains` method to determine if a specified cell falls
      within the radius

--- src/radius.lua
+++ src/radius.lua
@@ -1,22 +1,40 @@
-Radius = {center = {x = nil, y = nil}, radius = nil}
+Radius = {cells = {}, center = {x = nil, y = nil}, radius = nil}
 
 -- draw a radius at the specified coordinates
 function Radius:update(x, y, r)
     self.center.x = x
     self.center.y = y
     self.radius = r
+
+    -- algo starts
+    self:cell(self.center.x + self.radius, self.center.y)
+    self:cell(self.center.x - self.radius, self.center.y)
+    self:cell(self.center.x, self.center.y + self.radius)
+    self:cell(self.center.x, self.center.y - self.radius)
+end
+
+-- register a cell as within the radius
+function Radius:cell(x, y)
+    if not self.cells[x] then self.cells[x] = {} end
+
+    self.cells[x][y] = true
+end
+
+-- return true if x,y is among the cells within the radius
+function Radius:contains(x, y)
+    return self.cells[x] and self.cells[x][y]
 end
 
 -- reset the radius coordinates
 function Radius:clear()
+    self.cells = {}
     self.center = {}
     self.radius = nil
 end
 
+-- draw the radius to the map
 function Radius:draw()
-    if not self.center.x or not self.center.y then return end
-    spr(48, (self.center.x + self.radius) * 8, self.center.y * 8)
-    spr(48, (self.center.x - self.radius) * 8, self.center.y * 8)
-    spr(48, self.center.x * 8, (self.center.y + self.radius) * 8)
-    spr(48, self.center.x * 8, (self.center.y - self.radius) * 8)
+    for x, cell in pairs(self.cells) do
+        for y, _ in pairs(cell) do spr(48, x * 8, y * 8) end
+    end
 end
