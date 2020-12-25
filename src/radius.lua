16dc2343caff1257fce509e780d7da10d4d672b8
Chris Lane <chris@chris-allen-lane.com>
2020-12-24 19:55:13 -0500

    fix(Radius): dramatically improve performance
    Implement caching layer to reduce unnecessary recursive function calls
    and dramatically speed up our algorithm.

--- src/radius.lua
+++ src/radius.lua
@@ -1,4 +1,18 @@
-Radius = {cells = {}}
+Radius = {cells = {}, cache = {}}
+
+-- return true if we have already visited a cell with `m` movement remaining
+function Radius:cached(x, y, m)
+    -- return true if we've been here before
+    if self.cache[x] and self.cache[x][y] and self.cache[x][y][m] then
+        return true
+    end
+
+    -- otherwise, note that we've *now* been here, and return false
+    if not self.cache[x] then self.cache[x] = {} end
+    if not self.cache[x][y] then self.cache[x][y] = {} end
+    if not self.cache[x][y][m] then self.cache[x][y][m] = true end
+    return false
+end
 
 -- draw a radius at the specified coordinates
 function Radius:update(x, y, mvmt)
@@ -9,6 +23,10 @@ end
 
 -- TODO: account for "reachability"
 function Radius:search(x, y, mvmt)
+    if self:cached(x, y, mvmt) then return end
+
+    printh("visiting: " .. x .. ", " .. y .. " (" .. mvmt .. ")")
+
     for _, cell in pairs(Radius.near(x, y)) do
         if Cell.passable(cell.x, cell.y, Map.current) then
             self:append(cell.x, cell.y)
@@ -28,6 +46,7 @@ function Radius:append(x, y)
 end
 
 -- return the set of cells adjacent to the specified cell
+-- TODO: account for out-of-bounds cells
 function Radius.near(x, y)
     return {
         {x = x + 1, y = y},
@@ -37,17 +56,6 @@ function Radius.near(x, y)
     }
 end
 
--- filter to cells that are reachable with `move` movement points
---[[
-function Radius.reachable(cells, move)
-    local reach = {}
-    for _, cell in pairs(cells) do
-        if move >= cell.move then add(reach, cell) end
-    end
-    return reach
-end
---]]
-
 -- return true if x,y is among the cells within the radius
 function Radius:contains(x, y)
     return self.cells[x] and self.cells[x][y]
@@ -55,6 +63,7 @@ end
 
 -- reset the radius coordinates
 function Radius:clear()
+    self.cache = {}
     self.cells = {}
 end
 
