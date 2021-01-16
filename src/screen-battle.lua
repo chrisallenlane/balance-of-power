8220e0e8b5e99781819eabb055f0bc6f322e6817
Chris Lane <chris@chris-allen-lane.com>
2021-01-15 22:16:33 -0500

    fix(screen-battle): map advance bug
    Fix bug whereby maps would advance incorrectly when clearing a map.
    (This was accomplished by refactoring and DRY-ing out a block of code.)

--- src/screen-battle.lua
+++ src/screen-battle.lua
@@ -4,24 +4,13 @@ function Screens.battle.update()
     local p1, p2 = Units.remain(Map.current.units)
 
     -- TODO: handle 1-player, 2-player games
-    -- TODO: load the next map, unless we're on the final map
     if p1 == 0 then
         -- TODO: display a defeat banner
         Screens.load("defeat")
         return
     end
     if p2 == 0 then
-        -- load the next map until we run out of maps
-        -- TODO: load "interstitial" screen
-        local nxt = Map.num + 1
-        if nxt < #Map.defs then
-            Map.num = nxt
-            Map:load(Map.num)
-            -- TODO: display a victory banner
-            Screens.load("intr")
-        else
-            Screens.load("victory")
-        end
+        Map.advance()
         return
     end
 
