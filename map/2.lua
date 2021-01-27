abc4624049e4277a0293af52af241c70c2fc39c6
Chris Lane <chris@chris-allen-lane.com>
2021-01-27 09:27:09 -0500

    chore(maps): make minor adjustments
    Make minor map adjustments to account for the new `Info` bar.

--- map/2.lua
+++ map/2.lua
@@ -9,12 +9,12 @@ add(Map.defs, function ()
 
     cell = {x = 32, y = 0, w = 16, h = 32},
 
-    cursor = {x = 3, y = 28},
+    cursor = {x = 3, y = 27},
 
     units = {
+      Unit:new({spr = 80, player = 1, cell = {x = 3, y = 27}}),
       Unit:new({spr = 80, player = 1, cell = {x = 3, y = 28}}),
       Unit:new({spr = 80, player = 1, cell = {x = 3, y = 29}}),
-      Unit:new({spr = 80, player = 1, cell = {x = 3, y = 30}}),
       Unit:new({spr = 64, player = 2, cell = {x = 1, y = 1}}),
       Unit:new({spr = 64, player = 2, cell = {x = 1, y = 2}}),
       Unit:new({spr = 64, player = 2, cell = {x = 1, y = 3}}),
--- map/3.lua
+++ map/3.lua
@@ -9,10 +9,10 @@ add(Map.defs, function ()
 
     cell = {x = 48, y = 0, w = 32, h = 32},
 
-    cursor = {x = 28, y = 30},
+    cursor = {x = 28, y = 29},
 
     units = {
-      Unit:new({spr = 80, player = 1, cell = {x = 28, y = 30}}),
+      Unit:new({spr = 80, player = 1, cell = {x = 28, y = 29}}),
       Unit:new({spr = 64, player = 2, cell = {x = 9, y = 5}}),
     },
   }
