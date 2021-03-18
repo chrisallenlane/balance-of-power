69f3ca34bff8701894a1c56b41e7d885aa95efa4
Chris Lane <chris@chris-allen-lane.com>
2021-03-18 09:21:38 -0400

    refactor: title screen menus
    - Create `Menus.Title:update` and `Menus.Title:draw`
    
    - Move menu logic out of `screen-title.lua` and into the above, in order
      to be consistent with menu patterns elsewhere.

--- src/menu-title.lua
+++ src/menu-title.lua
@@ -1,1 +1,35 @@
 Menus.Title = {choices = {"1 player", "2 player", "skirmish"}, sel = 1}
+
+function Menus.Title:update(state, inputs)
+    -- up
+    if inputs.up:once() and self.sel >= 2 then
+        self.sel = self.sel - 1
+
+        -- down
+    elseif inputs.down:once() and self.sel <= 2 then
+        self.sel = self.sel + 1
+
+        -- "select"
+    elseif inputs.yes:once() then
+        -- flag P2 as a CPU if a 1-player game is selected
+        if self.sel == 1 then state.players[2].cpu = true end
+
+        -- load the map interstitial
+        -- TODO: handle this differently in 2-player mode
+        state.screen = Screens.intr
+    end
+end
+
+function Menus.Title:draw(_)
+    local y = 50
+
+    for i, choice in ipairs(self.choices) do
+        if i == self.sel then
+            print("* " .. choice, 20, y, 1)
+        else
+            print("  " .. choice, 20, y, 1)
+        end
+
+        y = y + 10
+    end
+end
--- src/screen-title.lua
+++ src/screen-title.lua
@@ -1,42 +1,12 @@
 -- update the title screen
 function Screens.title.update(state, inputs)
-    local menu = Menus.Title
-    local sel = menu.sel
-
-    -- up
-    if inputs.up:once() and sel >= 2 then
-        menu.sel = sel - 1
-
-        -- down
-    elseif inputs.down:once() and sel <= 2 then
-        menu.sel = sel + 1
-
-        -- "select"
-    elseif inputs.yes:once() then
-        -- flag P2 as a CPU if a 1-player game is selected
-        if menu.sel == 1 then state.players[2].cpu = true end
-
-        -- load the map interstitial
-        -- TODO: handle this differently in 2-player mode
-        state.screen = Screens.intr
-    end
+    Menus.Title:update(state, inputs)
 end
 
 -- draw the title screen
-function Screens.title.draw()
+-- TODO: this is a stub
+function Screens.title.draw(state)
     rectfill(0, 0, 127, 127, 12)
     String.centerX("balance of power", 30, 1)
-
-    local menu = Menus.Title
-    local y = 50
-
-    for i, choice in ipairs(menu.choices) do
-        if i == menu.sel then
-            print("* " .. choice, 20, y, 1)
-        else
-            print("  " .. choice, 20, y, 1)
-        end
-
-        y = y + 10
-    end
+    Menus.Title:draw(state)
 end
