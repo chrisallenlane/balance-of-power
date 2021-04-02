f79af803ea1f774661610b789783ed42d7133dbe
Chris Lane <chris@chris-allen-lane.com>
2021-04-02 14:58:26 -0400

    refactor(SFX): remove tokens
    Refactor `SFX` to save some tokens.

--- src/sfx.lua
+++ src/sfx.lua
@@ -1,12 +1,12 @@
 SFX = {
+    -- TODO: represent this with fewer tokens!
     -- map effect names to a table of play parameters
     fx = {['power-up'] = {0, 0, 8}, ['power-down'] = {0, 8, 8}},
 }
 
 -- Play the specified sound effect
-function SFX:play(fx)
-    local effect = self.fx[fx][1]
-    local offset = self.fx[fx][2]
-    local length = self.fx[fx][3]
-    sfx(effect, 3, offset, length)
+function SFX:play(name)
+    local effect = self.fx[name]
+    -- effect, channel, offset, length
+    sfx(effect[1], 3, effect[2], effect[3])
 end
