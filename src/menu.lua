708215e931995626557e39d8c71d12661da3f345
Chris Lane <chris@chris-allen-lane.com>
2021-06-11 11:05:17 -0400

    refactor(Menu): simplify `select`
    Simplify the implementation of `Menu.select`, and also save a few
    tokens.

--- src/menu.lua
+++ src/menu.lua
@@ -2,9 +2,9 @@ Menu = {}
 
 -- Implement common menu choice-selection functionality
 function Menu.select(self, inputs)
-    if inputs.up:rep() and self.sel >= 2 then
+    if inputs.up:rep() and self.sel > 1 then
         return self.sel - 1
-    elseif inputs.down:rep() and self.sel <= #self.choices - 1 then
+    elseif inputs.down:rep() and self.sel < #self.choices then
         return self.sel + 1
     end
     return self.sel
