8b87bfe331559eaa903217292114907f2f034a6f
Chris Lane <chris@chris-allen-lane.com>
2021-06-13 12:38:57 -0400

    refactor(Swarm): state reduction
    Refactor `Swarm` such that ships no longer track their own `deg` values.
    This change necessarily preceeds making swarm sizes adapt to unit
    health.

--- src/swarm.lua
+++ src/swarm.lua
@@ -5,12 +5,13 @@ function Swarm:new(x, y)
     local s = {
         pxx = x,
         pxy = y,
+        deg = 0,
         ships = {
-            {pxx = 0, pxy = 0, deg = 0},
-            {pxx = 0, pxy = 0, deg = .2},
-            {pxx = 0, pxy = 0, deg = .4},
-            {pxx = 0, pxy = 0, deg = .6},
-            {pxx = 0, pxy = 0, deg = .8},
+            {pxx = 0, pxy = 0},
+            {pxx = 0, pxy = 0},
+            {pxx = 0, pxy = 0},
+            {pxx = 0, pxy = 0},
+            {pxx = 0, pxy = 0},
         },
     }
 
@@ -24,13 +25,15 @@ end
 function Swarm:update(x, y)
     -- move the swarm with the unit
     self.pxx, self.pxy = x, y
+    self.deg = self.deg + 0.001
+    self.deg = self.deg > 1 and 0 or self.deg
 
     -- rotate each ship
-    for _, ship in pairs(self.ships) do
-        ship.deg = ship.deg + 0.001
-        ship.deg = ship.deg > 1 and 0 or ship.deg
-        ship.pxx = self.pxx + 4 + 4 * cos(ship.deg)
-        ship.pxy = self.pxy + 3 + 4 * sin(ship.deg)
+    for i, ship in pairs(self.ships) do
+        local offset = (1 / #self.ships) * i
+        -- NB: the +3/+4 center the ships in the cell
+        ship.pxx = self.pxx + 4 + 4 * cos(self.deg + offset)
+        ship.pxy = self.pxy + 3 + 4 * sin(self.deg + offset)
     end
 end
 
